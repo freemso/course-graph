22422af6f9fbc33320553accd868279255b368cb
zzxiong <tomcatxiong2@gmail.com>
2018-06-07 23:23:31 +0800

    add more query methods in course repo

--- course-graph-server/src/edu/fudan/main/repository/CourseRepository.java
+++ course-graph-server/src/edu/fudan/main/repository/CourseRepository.java
@@ -6,6 +6,8 @@ import edu.fudan.main.domain.CourseGraph;
 import edu.fudan.main.domain.Student;
 import edu.fudan.main.domain.Teacher;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
@@ -19,27 +21,83 @@ public interface CourseRepository extends Neo4jRepository<Course, Long> {
 
     //find course
     Optional<Course> findByName(String name);
+
     Optional<Course> findById(Long id);
+
     Optional<Course> findByCode(String code);
 
     @Query("MATCH (course:Course)<-[r:STUDENT_OF]-(student:Student) " +
             "WHERE course.courseId = {id} " +
-            "RETURN student")
-    Set<Student> getStudentsById(@Param("id") Long id);
+            "RETURN student ORDER BY student.name")
+    Set<Student> findStudentsById(@Param("id") Long id);
+
+    @Query(value = "MATCH (course:Course)<-[r:STUDENT_OF]-(student:Student) " +
+            "WHERE course.courseId = {id} " +
+            "RETURN student ORDER BY student.name",
+            countQuery = "MATCH (course:Course)<-[r:STUDENT_OF]-(student:Student) " +
+                    "WHERE course.courseId = {id} " +
+                    "RETURN count(*)")
+    Page<Student> findStudentsById(@Param("id") Long id, Pageable page);
+
 
     @Query("MATCH (course:Course)<-[r:STUDENT_OF]-(student:Student) " +
             "WHERE course.name = {name} " +
-            "RETURN student")
-    Set<Student>  getStudentsByName(@Param("name") String name);
+            "RETURN student ORDER BY student.name")
+    Set<Student> findStudentsByName(@Param("name") String name);
+
+    @Query(value = "MATCH (course:Course)<-[r:STUDENT_OF]-(student:Student) " +
+            "WHERE course.name = {name} " +
+            "RETURN student ORDER BY student.name",
+            countQuery = "MATCH (course:Course)<-[r:STUDENT_OF]-(student:Student) " +
+                    "WHERE course.name = {name} " +
+                    "RETURN count(*)")
+    Page<Student> findStudentsByName(@Param("name") String name, Pageable page);
+
 
     @Query("MATCH (course:Course)<-[r:STUDENT_OF]-(student:Student) " +
             "WHERE course.code = {code} " +
-            "RETURN student")
-    Set<Student>  getStudentsByCode(@Param("code") String code);
+            "RETURN student ORDER BY student.name")
+    Set<Student> findStudentsByCode(@Param("code") String code);
+
+    @Query(value = "MATCH (course:Course)<-[r:STUDENT_OF]-(student:Student) " +
+            "WHERE course.code = {code} " +
+            "RETURN student ORDER BY student.name",
+            countQuery = "MATCH (course:Course)<-[r:STUDENT_OF]-(student:Student) " +
+                    "WHERE course.code = {code} " +
+                    "RETURN count(*)")
+    Page<Student> findStudentsByCode(@Param("code") String code, Pageable page);
 
 
+    @Query("MATCH (course:Course)<-[r:TEACHER_OF]-(teacher:Teacher) " +
+            "WHERE course.id = {id} " +
+            "RETURN teacher")
+    Teacher findTeacherByCourseId(@Param("id") long id);
+
+    @Query("MATCH (course:Course)<-[r:TEACHER_OF]-(teacher:Teacher) " +
+            "WHERE course.code = {code} " +
+            "RETURN teacher")
+    Set<Teacher> findTeacherByCode(@Param("code") String code);
+
+    @Query("MATCH (course:Course)<-[r:TEACHER_OF]-(teacher:Teacher) " +
+            "WHERE course.name = {name} " +
+            "RETURN teacher")
+    Teacher findTeacherByName(@Param("name") String name);
 
 
+    @Query("MATCH (course:Course)<-[r:GRAPH_OF]-(courseGraph:CourseGraph) " +
+            "WHERE course.id = {id} " +
+            "RETURN courseGraph ORDER BY courseGraph.name")
+    List<CourseGraph> findGraphById(@Param("id") long id);
+
+    @Query("MATCH (course:Course)<-[r:GRAPH_OF]-(courseGraph:CourseGraph) " +
+            "WHERE course.name = {name} " +
+            "RETURN courseGraph ORDER BY courseGraph.name")
+    List<CourseGraph> findGraphByName(@Param("name") String name);
+
+    @Query("MATCH (course:Course)<-[r:GRAPH_OF]-(courseGraph:CourseGraph) " +
+            "WHERE course.code = {code} " +
+            "RETURN courseGraph ORDER BY courseGraph.name")
+    List<CourseGraph> findGraphByCode(@Param("code") String code);
 
 
 

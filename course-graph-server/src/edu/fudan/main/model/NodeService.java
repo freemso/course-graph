099297203f532ce6edb71e712f25acd1e00290c3
zzxiong <tomcatxiong2@gmail.com>
2018-06-13 20:52:45 +0800

    minor change

--- course-graph-server/src/edu/fudan/main/model/NodeService.java
+++ course-graph-server/src/edu/fudan/main/model/NodeService.java
@@ -1,11 +1,26 @@
 package edu.fudan.main.model;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NodeService {
 
+    
+
     public void deleteNode(long NodeId){
 
     }
+
+
+    private void updateNodes(String jsMindData){
+        JSONObject mindObject = new JSONObject(jsMindData);
+        List<String> newNodeIds = new ArrayList<>();
+
+    }
+
+
 }

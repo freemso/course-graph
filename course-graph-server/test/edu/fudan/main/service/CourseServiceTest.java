0f0db301788eb4cea09fb0ab9d7fd701e0a1aefb
zzxiong <tomcatxiong2@gmail.com>
2018-06-14 14:33:13 +0800

    add some test classes (only framework now)

--- course-graph-server/test/edu/fudan/main/service/CourseServiceTest.java
+++ course-graph-server/test/edu/fudan/main/service/CourseServiceTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import static org.junit.Assert.*;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest

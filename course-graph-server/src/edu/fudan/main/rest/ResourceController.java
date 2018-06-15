6955b8826c605b86f4881df7dcdcd1d93d156124
freemso <freemso@hotmail.com>
2018-06-15 20:27:17 +0800

    add resource controller

---
+++ course-graph-server/src/edu/fudan/main/rest/ResourceController.java
@@ -0,0 +1,11 @@
+package edu.fudan.main.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/resources/{rid}")
+public class ResourceController {
+}

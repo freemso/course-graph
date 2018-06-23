d8d8d2fab21071ae4ce93168d2e804571830ccc7
freemso <freemso@hotmail.com>
2018-06-23 20:10:51 +0800

    change mail

--- course-graph-server/src/main/java/edu/fudan/model/MailService.java
+++ course-graph-server/src/main/java/edu/fudan/model/MailService.java
@@ -3,20 +3,31 @@ package edu.fudan.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 @Service
 public class MailService {
 
     @Autowired
-    private JavaMailSender mailSender;
+    private JavaMailSenderImpl mailSender;
 
     @Value("${spring.mail.username}")
     private String from;
+
+    @Value("${mail.smtp.auth}")
+    private boolean auth;
+
+    @Value("${mail.smtp.starttls.enable}")
+    private boolean starttls;
+
+    @Value("${mail.smtp.starttls.required}")
+    private boolean startlls_required;
 
     private static final String TITLE_SIGN_UP = "Validation Code for CourseGraph Website Registration";
 
@@ -24,6 +35,12 @@ public class MailService {
 
     public void sendMessage(String mail, String message) {
         MimeMessage mailMessage = mailSender.createMimeMessage();
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", auth);
+        mailProperties.put("mail.smtp.starttls.enable", starttls);
+        mailProperties.put("mail.smtp.starttls.required", startlls_required);
+
+        mailSender.setJavaMailProperties(mailProperties);
         MimeMessageHelper helper = null;
         try {
             helper = new MimeMessageHelper(mailMessage, true, "GBK");
